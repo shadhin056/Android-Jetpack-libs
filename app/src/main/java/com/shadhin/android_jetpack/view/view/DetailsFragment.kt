3921ccb459fe1a712c53a3dd57fcd2919e011473
ShadhiN <shadhinemail@gmail.com>
2020-01-03 18:24:47 +0600

    menu just added :continue permission and sharing

--- app/src/main/java/com/shadhin/android_jetpack/view/view/DetailsFragment.kt
+++ app/src/main/java/com/shadhin/android_jetpack/view/view/DetailsFragment.kt
@@ -4,10 +4,8 @@ package com.shadhin.android_jetpack.view.view
 import android.graphics.Bitmap
 import android.graphics.drawable.Drawable
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModel
@@ -33,16 +31,17 @@ import kotlinx.android.synthetic.main.fragment_details.*
 class DetailsFragment : Fragment() {
     private lateinit var viewModel: DetailsViewModel
     private var dogUuidThis = 0;
-    private lateinit var dataBinding:FragmentDetailsBinding
+    private lateinit var dataBinding: FragmentDetailsBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        dataBinding= DataBindingUtil.inflate(inflater,R.layout.fragment_details,container,false)
+        setHasOptionsMenu(true)
+        dataBinding = DataBindingUtil.inflate(inflater, R.layout.fragment_details, container, false)
 
         // Inflate the layout for this fragment
-      //  return inflater.inflate(R.layout.fragment_details, container, false)
+        //  return inflater.inflate(R.layout.fragment_details, container, false)
         return dataBinding.root
     }
 
@@ -63,8 +62,8 @@ class DetailsFragment : Fragment() {
     private fun observeViewModel() {
         viewModel.dogLiveData.observe(this, Observer { dogs ->
             dogs?.let {
-              dataBinding.dog=dogs
-                it.imageUrl?.let{
+                dataBinding.dog = dogs
+                it.imageUrl?.let {
                     setUpBackgroundColor(it)
                 }
                 /*  txtDogName.text = dogs.dogBreed
@@ -77,26 +76,44 @@ class DetailsFragment : Fragment() {
             }
         })
     }
-    private fun setUpBackgroundColor(url:String){
+
+    private fun setUpBackgroundColor(url: String) {
         Glide.with(this)
             .asBitmap()
             .load(url)
-            .into(object :CustomTarget<Bitmap>(){
+            .into(object : CustomTarget<Bitmap>() {
                 override fun onLoadCleared(placeholder: Drawable?) {
 
                 }
 
                 override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
                     Palette.from(resource)
-                        .generate {
-                            palette ->
-                            val intColor=palette?.vibrantSwatch?.rgb ?:0
-                            val myPalatte=DogPalette(intColor)
-                            dataBinding.palette=myPalatte
+                        .generate { palette ->
+                            val intColor = palette?.vibrantSwatch?.rgb ?: 0
+                            val myPalatte = DogPalette(intColor)
+                            dataBinding.palette = myPalatte
                         }
                 }
 
             })
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        inflater.inflate(R.menu.details_menu,menu)
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when(item.itemId){
+            R.id.action_share->{
+
+            }
+            R.id.action_send_sms->{
+
+            }
+        }
+        return super.onOptionsItemSelected(item)
     }
 
 }
